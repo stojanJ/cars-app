da33c02fc99fb273f83be5854980884d5088a5b5
stojanJ <stojanjanjic91@gmail.com>
2022-11-20 22:21:59 +0100

    3 zadatak

--- src/pages/AppAddCar.jsx
+++ src/pages/AppAddCar.jsx
@@ -1,87 +1,119 @@
-import React from "react";
+import React, {useState} from "react";
 
-export default function AppAddCar({
-    newCar,
-    setNewCar,
-    onCreateNewCar,
-  })
-  {
-    const years = (start = 1990, end = 2018) => {
-        return Array.apply(0, Array(end - start + 1))
-            .map((element, index) => index + start);
+
+const years = (start = 1990, end = 2018) => {
+  return Array.apply(0, Array(end - start + 1))
+    .map((element, index) => index + start);
+}
+const ENGINES = ['diesel', 'petrol', 'electric', 'hybrid'];
+
+export default function AppAddCar() {
+
+  const [newCar, setNewCar] = useState({
+    brand: '',
+    model: '',
+    year: YEARS[0],
+    maxSpeed: '',
+    numberOfDoors: '',
+    isAutomatic: false,
+    engine: '',
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (id) {
+      await carService.edit(id, newCar);
+    } else {
+      await carService.add(newCar);
     }
 
-   return(
+    history.push('/cars');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          required
+          type="text"
+          aria-label="Bred"
+          value={newCar.brend}
+          onChange={({ target }) =>
+            setNewCar({ ...newCar, brend: target.value })
+          }
+        ></input>
+        <input
+          required
+          type="text"
+          aria-label="model"
+          value={newCar.model}
+          onChange={({ target }) =>
+            setNewCar({ ...newCar, model: target.value })
+          }
+        />
+        <select
+          required
+          value={newCar.year}
+          aria-label="year"
+          onChange={({ target }) =>
+            setNewCar({
+              ...newCar,
+              year: target.value,
+            })
+          }
+        > {years().map((year, index) => (
+          <option key={index} value={year}>{year}</option>
+        ))}
+        </select>
+        <input
+          required
+          type="integer"
+          aria-label="max speed"
+          value={newCar.maxSpeed}
+          onChange={({ target }) =>
+            setNewCar({ ...newCar, maxSpeed: target.value })
+          }
+        />
+        <input
+          required
+          aria-label="number of doors"
+          type="integer"
+          value={newCar.numberOfDoors}
+          onChange={({ target }) =>
+            setNewCar({ ...newCar, numberOfDoors: target.value })
+          }
+        />
+        <span>
+          <label>Is automatic?</label>
+          <input
+            type='checkbox'
+            checked={newCar.isAutomatic}
+            onChange={({ target }) => {
+              setNewCar({ ...newCar, isAutomatic: target.checked });
+            }}
+          />
+        </span>
+
         <div>
-            <form onSubmit={onCreateNewCar}>
-            <input
-                required
-                type="text"
-                aria-label="Bred"
-                value={newCar.brend}
-                onChange={({ target }) =>
-                setNewCar({ ...newCar, brend: target.value })
-                }
-            ></input>
-            <input
-                required
-                type="text"
-                aria-label="model"
-                value={newCar.model}
-                onChange={({ target }) =>
-                setNewCar({ ...newCar, model: target.value })
-                }
-            />
-            <select 
-              required
-              value={newCar.year}
-              aria-label="year"
-              onChange={({ target }) =>
-                setNewCar({
-                  ...newCar,
-                  year: target.value,
-                })
-              }
-            > {years().map((year, index) => (
-                <option key={index} value={year}>{year}</option>
-            ))}
-            </select>
-            <input
-                required
-                type="integer"
-                aria-label="max speed"
-                value={newCar.maxSpeed}
-                onChange={({ target }) =>
-                setNewCar({ ...newCar, maxSpeed: target.value })
-                }
-            />
+          <h4>Pick engine:</h4>
+          {ENGINES.map((engine, index) => (
+            <span key={index}>
               <input
+                type='radio'
+                name='engine'
                 required
-                aria-label="number of doors"
-                type="integer"
-                value={newCar.numberOfDoors}
-                onChange={({ target }) =>
-                setNewCar({ ...newCar, numberOfDoors: target.value })
-                }
-            />
-               <input
-                type="radio"
-                aria-label="is automatic"
-                value={checked}
-                onChange={({ target }) =>
-                setNewCar({ ...newCar, isAutomatic: target.value })
-                }
-            />
-                <input
-                type="radio"
-                aria-label="is automatic"
-                value={newCar.isAutomatic}
-                onChange={({ target }) =>
-                setNewCar({ ...newCar, isAutomatic: target.value })
-                }
-            />           
-            <button type="submit">Submit</button>
-            </form>
-            </div>
-    )
+                checked={engine === newCar.engine}
+                value={engine}
+                onChange={() => setNewCar({ ...newCar, engine })}
+              />
+              {engine.toUpperCase()}
+            </span>
+          ))}
+        </div>
+        <div>
+          <button type="submit" >Submit</button>
+        </div>
+      </form>
+    </div>
+  )
 }
--- src/services/CarsService.js
+++ src/services/CarsService.js
@@ -1,11 +1,22 @@
 import { axiosInstance } from "./AxiosService";
 
-class CarsService{
-     async getAll() {
-            const response = await axiosInstance.get("/api/cars");
-            return response.data;
-          }
+class CarsService {
+  async getAll() {
+    const response = await axiosInstance.get("/api/cars");
+    return response.data;
+  }
 
+  async add(newCar) {
+    try {
+      const data  = await axiosInstance.post('api/cars', newCar);
+
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+
+    return null;
+  }
 }
 
 export default new CarsService();
